dadc617b67918a966f7ca00c7cebd5b3b7039abe
Mason Burrill <115730350+burrillmj@users.noreply.github.com>
2023-04-21 17:15:58 -0600

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-print("Hello, this is a band name generator!")
-city = input("First, what was the city that you grew up in?\n")
-pet = input("Alright, next what is one of your pet's nams?\n")
-print("Cool, your band name is The " + city + ' ' + pet + "'s")
