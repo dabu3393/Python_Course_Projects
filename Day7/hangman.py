b298f8baeef19054263267b874e1ce4627045966
Davis Burrill <davisburrill@icloud.com>
2023-04-25 01:46:55 -0600

    Final version of hangman

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,30 +1,51 @@
-import random
+import hangman_art
+import requests
 
-word_list = ['pencil', 'avalanche', 'aluminum', 'fencing']
+api_url = 'https://random-word-api.herokuapp.com/word'
+word_api = requests.get(api_url).json()
 
-chosen_word = random.choice(word_list)
+print(hangman_art.logo)
+chosen_letters = []
+chosen_word = word_api[0]
+word_length = len(chosen_word)
+lives = 6
 
 display = []
 
-for blank in chosen_word:
-    display.append('_')
+for _ in range(word_length):
+    display += '_'
 
-print(display)
+print(f"{' '.join(display)}")
 
 end_of_game = False
 
 while not end_of_game:
 
-    guessed_letter = input('What letter do you guess?\n').lower()
+    guessed_letter = input('Guess a letter: ').lower()
 
-    for position in range(len(chosen_word)):
+    chosen_letters += guessed_letter
+
+    if guessed_letter in display:
+        print(f'You\'ve already guessed {guessed_letter}\n')
+
+    print(f"\nGuessed letters: {', '.join(chosen_letters)}\n")
+    
+    if guessed_letter not in chosen_word:
+        lives -= 1
+        print(f'You guessed {guessed_letter}. That\'s not in the word.')
+        if lives == 0:
+            end_of_game = True
+            print(f'You lose. The word was {chosen_word}.')
+
+    for position in range(word_length):
         letter = chosen_word[position]
-        print(f'Current position: {position}\nCurrent letter: {letter}\nGuessed letter: {guessed_letter}')
+        # print(f'Current position: {position}\nCurrent letter: {letter}\nGuessed letter: {guessed_letter}')
         if letter == guessed_letter:
             display[position] = letter
-        
 
-    print(display)
+    print(f"{' '.join(display)}\n")
+
+    print(hangman_art.stages[lives])
 
     if '_' not in display:
         end_of_game = True
