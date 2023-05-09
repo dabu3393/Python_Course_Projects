25c1397eac89a3cb3488805e5e717284a44dde05
Davis Burrill <davisburrill@icloud.com>
2023-05-08 23:35:45 -0600

    clean up data

--- Day17/data.py
+++ Day17/data.py
@@ -7,31 +7,76 @@ question_data = [
         "correct_answer": "False",
         "incorrect_answers": ["True"]
     },
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "Skateboarding will be included in the 2020 Summer Olympics in Tokyo.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "Tennis was once known as Racquetball.", "correct_answer": "False",
-     "incorrect_answers": ["True"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "The Olympics tennis court is a giant green screen.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "easy",
-     "question": "Roger Federer is a famous soccer player.", "correct_answer": "False",
-     "incorrect_answers": ["True"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "Formula E is an auto racing series that uses hybrid electric race cars.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "easy",
-     "question": "In Rugby League, performing a &quot;40-20&quot; is punished by a free kick for the opposing team.",
-     "correct_answer": "False", "incorrect_answers": ["True"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "Wilt Chamberlain scored his infamous 100-point-game against the New York Knicks in 1962.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "During Wimbledon, spectators in the grounds can buy the tennis balls that have been used in matches.",
-     "correct_answer": "True", "incorrect_answers": ["False"]},
-    {"category": "Sports", "type": "boolean", "difficulty": "medium",
-     "question": "In 2008, Usain Bolt set the world record for the 100 meters with one shoelace untied.",
-     "correct_answer": "True", "incorrect_answers": ["False"]}
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Skateboarding will be included in the 2020 Summer Olympics in Tokyo.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Tennis was once known as Racquetball.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Olympics tennis court is a giant green screen.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Roger Federer is a famous soccer player.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Formula E is an auto racing series that uses hybrid electric race cars.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Rugby League, performing a &quot;40-20&quot; is punished by a free kick for the opposing team.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Wilt Chamberlain scored his infamous 100-point-game against the New York Knicks in 1962.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "During Wimbledon, spectators in the grounds can buy the tennis balls that have been used in matches.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In 2008, Usain Bolt set the world record for the 100 meters with one shoelace untied.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
 ]
