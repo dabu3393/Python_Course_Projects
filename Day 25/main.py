19922d64a85c75e4eaca3389bf3b6b0bd79baea9
Davis Burrill <davisburrill@icloud.com>
2023-05-18 00:07:02 -0600

    Day 25 update, creating us map game

--- Day 25/main.py
+++ Day 25/main.py
@@ -17,10 +17,12 @@ while len(guessed_states) < 50:
                                     prompt='Enter a state\'s name').title()
     # Create an exit keyword to exit the game when you are finished
     if answer_state == 'Exit':
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(all_states)
+        new_data.to_csv('states_to_learn.csv')
         break
     # Write state over correct state if guessed
     if answer_state in all_states:
-        all_states.remove(answer_state)
         guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
