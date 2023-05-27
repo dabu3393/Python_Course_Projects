412dc782d6117ff8d497b1ad531885765e94039f
Davis Burrill <davisburrill@icloud.com>
2023-05-27 15:36:26 -0600

    Day 32 update, created an automatic birthday wisher that sends an email out wishing them a happy birthday on their birthday. Use pythonanywhere to automate it to run every day and check if it is someone's birthday.

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,4 +1,3 @@
-##################### Extra Hard Starting Project ######################
 import datetime as dt
 import smtplib
 
@@ -30,13 +29,6 @@ if today in birthday_dict:
             msg=f'Subject:Happy Birthday!\n\n{contents}'
         )
 
-
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
 
 
 
